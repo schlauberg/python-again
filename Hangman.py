a033a9b11a20b3f79cc23c4f02093f115458a5a3
Achim miri <github.com@heger.li>
2022-03-24 16:05:59 +0100

    hangman in python

---
+++ Hangman.py
@@ -0,0 +1,9 @@
+# Hangman
+import random
+blanked_word = []
+word_list = ["aardvark","baboon","camel", "apple", "banana", "rawberry"]
+chosen_word = random.choice(word_list)
+
+for i in range(0,len(chosen_word))
+    blanked_word[i].append("_")
+print(f"Word is: {chosen_word} blanked word is: {blanked_word}")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-again
 pythonic
+Just some repo for refreshing my python knowledge from 2 to 3 and other low key stuff
