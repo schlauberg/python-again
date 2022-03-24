6e8865da1e851e4eaea9db25e48f74c57ca70b63
miri <github.com@heger.li>
2022-03-24 16:32:42 +0100

    blanked

--- Hangman.py
+++ Hangman.py
@@ -5,5 +5,5 @@ word_list = ["aardvark","baboon","camel", "apple", "banana", "rawberry"]
 chosen_word = random.choice(word_list)
 
 for i in range(0,len(chosen_word)-1):
-    blanked_word[i].append("_")
-print(f"Word is: {chosen_word} blanked word is: {blanked_word}")
+    blanked_word+=('_')
+print(f"Len is {len(chosen_word)} word is: {chosen_word} blanked word is: {blanked_word}")
